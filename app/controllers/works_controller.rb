41a6bf9c11219771dc8e16f470fc6b0966bb631a
Prelang Builder <builder@prelang.com>
2014-06-12 00:32:53 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,5 @@
 class WorksController < ApplicationController
-  before_action :set_work, only: [:show, :edit, :update, :destroy]
+  before_action :set_work, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /works
   # GET /works.json
